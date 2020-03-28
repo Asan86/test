98d78e97b6755a9fca6a64eb956111e011943abc
Asan86 <asanturdukulov86@gmail.com>
2020-03-29 03:21:22 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+      int[] number = new int[4];
+        for (int i = 0; i < number.length; i++) {
+
+        }
+        int [] numders1 = {1,2,3,4,5};
+        numders1[0]=3;
+        numders1[1]=5;
+        numders1[2]=10;
+        numders1[3]=15;
+        numders1[4]=20;
+        for (int i = 0; i <numders1.length ; i++) {
+
+        }
+        System.out.println(number.length);
+
+    }
+}
